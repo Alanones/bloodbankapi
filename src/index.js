b3db96c3ff9661bf74ea6610f25ff6ff666d7438
Alanones <109990442+Alanones@users.noreply.github.com>
2023-06-06 11:02:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-const cors = require('cors');
 const app = require("./app");
-
-const corsOptions ={
-   origin:'*', 
-   credentials:true,           
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) 
-
 const port = process.env.PORT;
 app.listen(port, () => console.log("Server Started on port ", port));
